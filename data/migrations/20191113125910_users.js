d7f0576a7d923bb7db0170ce5e4e1e074c96f558
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-11-13 14:24:31 -0800

    model and router setup and test

--- data/migrations/20191113125910_users.js
+++ data/migrations/20191113125910_users.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema.createTable('users', tb => {
+		tb.increments();
+
+		tb.string('username', 128)
+			.notNullable()
+			.unique();
+		tb.string('password', 128).notNullable();
+		tb.string('department', 255);
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('users');
 };
